fc911e5b95129e82bd5b8e66520e530c948233de
Truong Tao Q. VN.Danang <truong.tao@MBA020236P.local>
2022-08-05 16:48:22 +0700

    update

--- pages/list-order.tsx
+++ pages/list-order.tsx
@@ -2,18 +2,24 @@ import axios from 'axios';
 import { GetStaticProps } from 'next';
 import * as React from 'react';
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+
 export interface ListOrderPageProps {
   order: any[]
 }
-
+const fetcher = async () => {
+  const response = await fetch('https://6274e2bf345e1821b230ebee.mockapi.io/orders');
+  return await response.json();
+};
 export default function ListOrderPage({ order }: ListOrderPageProps) {
   console.log(order);
+  const { data, error } = useSWR('https://6274e2bf345e1821b230ebee.mockapi.io/orders', fetcher)
 
   return (
     <>
       <h1>Hello</h1>
       {
-        order.map((item: any) => (
+        data.map((item: any) => (
           <>
             <h1>Order{item.id}</h1>
             <h1>{item.isPaid.toString()}</h1>
@@ -30,6 +36,7 @@ export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       order: data.data
-    }
+    },
+    revalidate: 5
   }
 }
